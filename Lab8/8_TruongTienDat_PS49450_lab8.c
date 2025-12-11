d19c1aa461457918aa1a888aa75ef224c0875392
tiendat <datdz140707@gmail.com>
2025-12-11 21:33:27 +0700

    Add files via upload

--- Lab8/8_TruongTienDat_PS49450_lab8.c
+++ Lab8/8_TruongTienDat_PS49450_lab8.c
@@ -1,8 +1,8 @@
 #include<stdio.h>
 #include<string.h>
-struct SinhVien{
+struct HOCVIEN{
+    int MSHV;
     char hoTen[50];
-    int tuoi;
     float diem;
     char hocLuc[10];
 };
@@ -10,44 +10,36 @@ int main(){
     int x,i,j;
     printf("nhap vao so sinh vien muon nhap:");
     scanf("%d",&x);
-    struct SinhVien sv[x];
+    struct HOCVIEN hv[x];
     for(i=0;i<x;i++){
         printf("nhap vao thong tin sinh vien thu %d:\n",i+1);
-        printf("ten:");
-        scanf("%s",sv[i].hoTen);
-        printf("tuoi:");
-        scanf("%d",&sv[i].tuoi);
+        printf("MSVH:");
+        scanf("%d",&hv[i].MSHV);
+        printf("ho va ten:");
+        scanf("%s",hv[i].hoTen);
         printf("diem:");
-        scanf("%f",&sv[i].diem);}
+        scanf("%f",&hv[i].diem);
 
         //tinh hoc luc
-        if(sv[i].diem>=9){
-            strcpy(sv[i].hocLuc,"Xuat sac");
-        }else if(sv[i].diem>=8){
-            strcpy(sv[i].hocLuc,"Gioi");
-        }else if(sv[i].diem>=7){
-            strcpy(sv[i].hocLuc,"Kha");
-        }else if(sv[i].diem>=5){
-            strcpy(sv[i].hocLuc,"Trung binh");
+        if(hv[i].diem>=9){
+            strcpy(hv[i].hocLuc,"Xuat sac");
+        }else if(hv[i].diem>=8){
+            strcpy(hv[i].hocLuc,"Gioi");
+        }else if(hv[i].diem>=7){
+            strcpy(hv[i].hocLuc,"Kha");
+        }else if(hv[i].diem>=5){
+            strcpy(hv[i].hocLuc,"Trung binh");
         }else{
-            strcpy(sv[i].hocLuc,"Yeu");
+            strcpy(hv[i].hocLuc,"Yeu");
         }
-        //sap xep diem
-        for(i=0;i<x;i++){
-            for(j=i+1;j<x;j++){
-                if(sv[i].diem<sv[j].diem){
-                    struct SinhVien temp=sv[i];
-                    sv[i]=sv[j];
-                    sv[j]=temp;
-                }
-            }
-        }
+    }
         //in ra danh sach sinh vien
         printf("danh sach sinh vien \n");
-        printf("\nHo ten\tTuoi\tDiem\tHoc luc\n");
+        printf("\nSTT\tMSHV\tHo ten\tDiem\tHoc luc\n");
         for(i=0;i<x;i++){
-            printf("%s\t%d\t%.1f\t%s");
+            printf("%d\t %d\t %s\t %.1f\t %s\n",i+1,hv[i].MSHV,hv[i].hoTen,hv[i].diem,hv[i].hocLuc);
         }
+
         return 0;
         }   
 
